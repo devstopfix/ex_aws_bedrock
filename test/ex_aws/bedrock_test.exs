f6d82d66120040b6f072f93ad2784382673a88df
J Every <devstopfix@gmail.com>
2024-01-13 13:43:20 +0000

    Operation get foundation model

--- test/ex_aws/bedrock_test.exs
+++ test/ex_aws/bedrock_test.exs
@@ -1,8 +1,35 @@
 defmodule ExAws.BedrockTest do
   use ExUnit.Case
-  doctest ExAws.Bedrock
+  alias ExAws.Bedrock
+  alias ExAws.Operation.JSON
 
-  test "greets the world" do
-    assert ExAws.Bedrock.hello() == :world
+  @model_id "amazon.titan-text-lite-v1"
+
+  describe "get_foundation_model/1" do
+    @tag :aws
+    test "from AWS", %{request: request} do
+      assert %{
+               "modelDetails" => %{
+                 "modelId" => @model_id
+               }
+             } = ExAws.request!(request)
+    end
+
+    test "http get", %{request: request} do
+      assert %JSON{http_method: :get} = request
+    end
+
+    test "path", %{request: request} do
+      assert %JSON{path: "/foundation-models/amazon.titan-text-lite-v1"} = request
+    end
+
+    test "service", %{request: request} do
+      assert %JSON{service: :bedrock} = request
+    end
+
+    setup do
+      request = Bedrock.get_foundation_model(@model_id)
+      {:ok, [request: request]}
+    end
   end
 end
