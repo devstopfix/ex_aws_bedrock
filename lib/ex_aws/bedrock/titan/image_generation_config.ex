80cb97bb8801f0b6502fc288529597967b13cfbe
J Every <devstopfix@gmail.com>
2024-01-14 15:43:56 +0000

    FIx for Elixir 1.12 (cannot use Map.filter)

--- lib/ex_aws/bedrock/titan/image_generation_config.ex
+++ lib/ex_aws/bedrock/titan/image_generation_config.ex
@@ -5,9 +5,20 @@ defmodule ExAws.Bedrock.Titan.ImageGenerationConfig do
   [AWS API Docs](https://docs.aws.amazon.com/bedrock/latest/userguide/model-parameters-titan-image.html)
   """
 
+  @type quality :: :standard | :premium
+
+  @type t :: %__MODULE__{
+          cfgScale: float(),
+          height: pos_integer(),
+          numberOfImages: pos_integer(),
+          quality: quality(),
+          seed: integer(),
+          width: pos_integer()
+        }
+
   @derive Jason.Encoder
   defstruct numberOfImages: 1,
-            quality: "standard",
+            quality: :standard,
             height: 1024,
             width: 1024,
             cfgScale: 8.0,
--- lib/ex_aws/bedrock/titan/text_to_image_params.ex
+++ lib/ex_aws/bedrock/titan/text_to_image_params.ex
@@ -1,14 +1,22 @@
 defmodule ExAws.Bedrock.Titan.TextToImageParams do
-  @moduledoc false
+  @moduledoc """
+  Text to image generation.
+
+  * text (Required) – A text prompt to generate the image.
+  * negativeText (Optional) – A text prompt to define what not to include in the image.
+  """
+
+  @type t :: %__MODULE__{
+          text: String.t(),
+          negativeText: String.t() | nil
+        }
 
   defstruct text: "", negativeText: nil
 
   defimpl Jason.Encoder, for: __MODULE__ do
     def encode(value, opts) do
-      value
-      |> Map.from_struct()
-      |> Map.filter(fn {_, v} -> v != nil end)
-      |> Jason.Encode.map(opts)
+      encoded = for {k, v} <- Map.from_struct(value), v != nil, into: %{}, do: {k, v}
+      Jason.Encode.map(encoded, opts)
     end
   end
 end
